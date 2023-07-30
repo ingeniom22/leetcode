79528d65fccd6df2d25803f4310b401f01f41cad
ingeniom22 <36503181+ingeniom22@users.noreply.github.com>
2023-07-30 22:26:05 +0700

    Time: 59 ms (74.6%), Space: 16.4 MB (44.63%) - LeetHub

---
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.py
@@ -0,0 +1,15 @@
+class Solution:
+    def selfDividingNumbers(self, left: int, right: int) -> List[int]:
+        def is_self_dividing(num: int):
+            for i in [int(x) for x in str(num)]:
+                if i == 0:
+                    return False
+                if num % i != 0:
+                    return False
+            return True
+        
+        result = []
+        for i in range(left, right+1):
+            if is_self_dividing(i):
+                result.append(i)
+        return result
